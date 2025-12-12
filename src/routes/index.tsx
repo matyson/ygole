cd902cdf7c613d903cd94440ea0886448e005f87
Matheus Bernardi <matheus_luis.bernardi@maxiv.lu.se>
2025-12-12 16:59:11 +0100

    add button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount((c) => c + 1)
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      hello
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <Button variant="outline" onClick={increment}>
+        Click me
+      </Button>
+      <p>Count: {count}</p>
     </div>
   )
 }
